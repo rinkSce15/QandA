027dbe87a9bb770f5561ebd043a22e3fe1600f22
rinkSce15 <96963814+rinkSce15@users.noreply.github.com>
2022-01-26 12:12:07 -0500

    Update solution.py

--- fuzzy_phrases/solution.py
+++ fuzzy_phrases/solution.py
@@ -2,7 +2,34 @@ import json
 
 def phrasel_search(P, Queries):
     # Write your solution here
-    ans = [[]]
+    
+        result =[]
+        print("Answer as below")
+        for item in Queries:
+            tempresult =[]
+            for pitem in P:
+               string = pitem.split(" ")
+               pattern = pitem  # Original Phrase
+               pattern1 = string[0] + '\s\w+\s' + string[1] #Fuzzy Phrase 1
+               if len(string) >= 3:
+                   pattern2 = string[0] +" " + string[1]+" " + string[2] + '\s\w+\s'+ string[3]  #Fuzzy Phrase 2
+               
+               ans = re.findall(pattern,item)
+               for val in ans:
+                   if val not in tempresult:
+                        tempresult.append(val)
+               
+               ans = re.findall(pattern1,item)
+               for val in ans:
+                   if val not in tempresult:
+                        tempresult.append(val)
+             
+               ans = re.findall(pattern2,item)  
+               for val in ans:
+                   if val not in tempresult:
+                        tempresult.append(val)
+            result.append(tempresult)
+    ans = result
     return ans
 
 if __name__ == "__main__":
